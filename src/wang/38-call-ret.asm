7eecafd1ef1ed37a85f6de948188455a9339fc6e
hing1 <hing123@126.com>
2020-01-01 23:52:12 +0800

    create 38 call ret.asm

---
+++ src/wang/38-call-ret.asm
@@ -0,0 +1,24 @@
+assume cs:codesg, ss:stacksg
+
+stacksg segment                 ; assume that the program is start from 1000:0
+	db 8 dup (0)
+	db 8 dup (0)
+stack ends
+
+codesg segment
+start:
+	mov ax, stacksg
+	mov ss, ax
+	mov sp, 16
+
+	mov ax, 1000
+	call s
+
+	mov ax, 4c00h
+	int 21h
+s:
+	add ax, ax
+	ret
+codesg ends
+end start
+
